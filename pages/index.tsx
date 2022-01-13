71b41ae1fc55c10f9f5924ce6356b52ffe591afa
Dylan <dylankilkenny95@gmail.com>
2022-01-13 19:01:42 +0000

    write funcs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import Head from 'next/head';
-import { useEthers } from '@usedapp/core';
+import { useContractFunction, useEthers } from '@usedapp/core';
 import dynamic from 'next/dynamic';
 import { ethers } from 'ethers';
 import FrostiesABI from '../abi/frosties.json';
+import WrappedFrostiesABI from '../abi/wrapped.json';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const frostiesMainnetAddress = '0x3c99f2a4b366d46bcf2277639a135a6d1288eceb';
-const frostiesRinkebyAddress = '';
+const wrappedMainnetAddress = '0xe905fccfb6fd93cc81cd5e2f544078bd2cfcc316';
 
-const wrappedMainnetAddress = '';
-const wrappedRinkebyAddress = '';
+const frostiesRinkebyAddress = '0xefe602390551b41ad24d28b2b4161023adb45803';
+const wrappedRinkebyAddress = '0xe905fccfb6fd93cc81cd5e2f544078bd2cfcc316';
 
 const WalleButton = dynamic(
   () => {
@@ -28,40 +29,74 @@ export default function App() {
   const [metadata, setMetadata] = useState<Metadata[]>([]);
   const [oldContract, setOldContract] = useState<string>();
   const [newContract, setNewContract] = useState<string>();
+  const [tokenIDs, setTokenIDs] = useState<number[]>();
+  const [approved, setApproved] = useState<boolean>(false);
   const { account, chainId, library } = useEthers();
 
   useEffect(() => {
+    console.log('chainId:', chainId);
+
     setOldContract(chainId === 1 ? frostiesMainnetAddress : frostiesRinkebyAddress);
     setNewContract(chainId === 1 ? wrappedMainnetAddress : wrappedRinkebyAddress);
   }, [chainId]);
 
+  // useEffect(() => {
+  //   if (account && newContract) {
+  //     const IWrappedFrosties = new ethers.utils.Interface(WrappedFrostiesABI);
+  //     const frostiesWrappedContract = new ethers.Contract(newContract, IWrappedFrosties, library?.getSigner());
+  //     const approved = await frostiesWrappedContract.
+  //   }
+  // }, [account, newContract]);
+
   useEffect(() => {
     async function fetchMetadata() {
-      console.log('account:', account);
-      if (account && oldContract) {
-        const IFrosties = new ethers.utils.Interface(FrostiesABI);
-        const frostiesContract = new ethers.Contract(oldContract, IFrosties, library);
+      try {
+        console.log('account:', account);
+        console.log('Frosties Address:', oldContract);
+        console.log('Frosties Wrapped:', newContract);
+        if (account && oldContract && newContract) {
+          const IFrosties = new ethers.utils.Interface(FrostiesABI);
+          const frostiesContract = new ethers.Contract(oldContract, IFrosties, library);
 
-        const IDs = await frostiesContract.walletOfOwner(account);
-        console.log('Frostie IDs', IDs);
+          const approved = await frostiesContract.isApprovedForAll(account, newContract);
+          console.log('approved', approved);
 
-        const meta = [];
-        for (let id of IDs) {
-          const uri: string = await frostiesContract.tokenURI(id);
-          console.log(`Metadata URI for ${id}: ${uri}`);
+          const IDs = await frostiesContract.walletOfOwner(account);
+          setTokenIDs(IDs.map((x: string) => parseInt(x)));
+          console.log('Frostie IDs', IDs);
 
-          const ipfsURL = 'https://ipfs.io/ipfs/' + uri.split('://')[1];
-          const resp = await axios(ipfsURL);
+          const meta = [];
+          for (let id of IDs) {
+            const uri: string = await frostiesContract.tokenURI(id);
+            console.log(`Metadata URI for ${id}: ${uri}`);
 
-          meta.push({ tokenId: id, image: 'https://ipfs.io/ipfs/' + resp.data.image.split('://')[1] });
+            const ipfsURL = 'https://ipfs.io/ipfs/' + uri.split('://')[1];
+            const resp = await axios(ipfsURL);
+
+            meta.push({ tokenId: id, image: 'https://ipfs.io/ipfs/' + resp.data.image.split('://')[1] });
+          }
+
+          setMetadata(meta);
+          setApproved(approved);
         }
-
-        setMetadata(meta);
+      } catch (error) {
+        console.log('Loading Error:', error);
       }
     }
     void fetchMetadata();
-  }, [account, oldContract]);
+  }, [account, oldContract, newContract]);
 
+  async function wrapAll() {
+    if (!approved && oldContract && newContract) {
+      const IFrosties = new ethers.utils.Interface(FrostiesABI);
+      const frostiesContract = new ethers.Contract(oldContract, IFrosties, library?.getSigner());
+      await frostiesContract.setApprovalForAll(newContract, true);
+    } else if (newContract) {
+      const IWrappedFrosties = new ethers.utils.Interface(WrappedFrostiesABI);
+      const frostiesWrappedContract = new ethers.Contract(newContract, IWrappedFrosties, library?.getSigner());
+      await frostiesWrappedContract.wrap(tokenIDs);
+    }
+  }
   return (
     <div>
       <Head>
@@ -78,8 +113,11 @@ export default function App() {
       <div className="w-96 m-auto">
         <div className="grid grid-cols-2 mt-24">
           <div className="col-span-1">
-            <button className="shadow-md flex justify-center w-44 py-2 mb-4 border text-white bg-blue-600  border-blue-700 rounded-lg cursor-pointer hover:bg-blue-500">
-              <span>Wrap All Frosties</span>
+            <button
+              onClick={wrapAll}
+              className="shadow-md flex justify-center w-44 py-2 mb-4 border text-white bg-blue-600  border-blue-700 rounded-lg cursor-pointer hover:bg-blue-500"
+            >
+              {approved ? <span>Wrap All Frosties</span> : <span>Approve Wrapper</span>}
             </button>
           </div>
           <div className="col-span-1">
